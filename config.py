d0b27d3a1db73961878f0185427e21baf17c2192
marcogiorgio24 <85278684+marcogiorgio24@users.noreply.github.com>
2021-06-05 16:30:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 w = open('config.json', 'w+')
 w.write('{')
 w.write('\n')
-w.write('    "authToken": 1808344776:AAGj2CBJEiCaUrz8Fiiowku0GzU9vBurxkY')
+w.write('    "authToken": 1826108127:AAG-74LmbMqWz8CWX7_lhvh_M-5vV53aia8')
 w.write('\n')
 w.write('    "owner": 780200564')
 w.write('\n')
